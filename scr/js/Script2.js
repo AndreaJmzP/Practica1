129cabcda480afe8329eeb864b9de4f07e6bd73a
Andy J <andreajimenezp.0582@gmail.com>
2025-02-05 10:17:54 -0600

    Practica

---
+++ scr/js/Script2.js
@@ -0,0 +1,81 @@
+//Asignación de variable
+let numero=56;
+
+//Expresión aritmética
+let multiplicacion=5*32;
+
+//Expresión algebraica
+let x;
+let algebraica= 5(x)-3(x)+1;
+
+//Llamada a función
+sumar();
+calcularPromedio();
+
+//Estructura de una función
+function sumar(x,y){
+
+}
+
+//Estructura de una función con valor de retorno
+function getPromedio(){
+
+    return promedio,x,y;
+}
+function getMessage(){
+    return "Holap";
+}
+//Estructura selectiva simple
+if(condicion){
+
+}
+
+//Estructura selectiva doble
+if(condicion){
+
+}else{
+
+}
+
+//Estructura selectiva múltiple
+if(condicion){
+
+}else if(condicion){
+
+}else{
+
+}
+
+//Bucles
+for(let i=0; i=10;i++){
+
+}
+
+//Estructura de una clase
+class MiClase{
+    nombre="andy";
+    edad="22";
+
+    constructor(apMaterno, apPaterno,gender){
+        this.apMaterno=apMaterno;
+        this.apPaterno=apPaterno;
+        this.gender="F";
+    }
+}
+
+//Manipulación de objetos y propiedades
+persona.numControl="22900271";
+
+//Creación de onjeto
+const objVehiculo={
+    color:"rojo",
+    modelo:"Vento",
+    marca:"Ferrari",
+    km: 345,
+    mostrarDatos: function (){
+        comsole.log ("Yo muestro los datos");
+    }
+};
+objVehiculo.mostrarDatos();
+
+console.log (objVehiculo.mostrarDatos)
---
+++ scr/js/scriptc.js
@@ -0,0 +1,11 @@
+//Creación de onjeto
+const objVehiculo={
+    color:"rojo",
+    modelo:"Vento",
+    marca:"Ferrari",
+    km: 345,
+    mostrarDatos: function (){
+        comsole.log ("Yo muestro los datos");
+    }
+};
+objVehiculo.mostrarDatos();
